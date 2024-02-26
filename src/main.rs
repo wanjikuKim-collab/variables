c6a2467fa835fb3bb1d69f8f7af583cc43481d81
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-02-26 15:45:09 +0300

    mutability in rust

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // variables mutability - by defaulkt they are immutable
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x= 6;
+    println!("The value of x is: {}", x);
+}
